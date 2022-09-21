a8b3f20d4a74c78cd5033edfc15a9c1346d2d658
Alex Contreras <alex.code29@gmail.com>
2022-09-21 17:04:43 -0500

    style: style header with classnames

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 
 function Header() {
     return (
-        <>
-        
-        </>
+        <header>
+            <h1 className='page-title'>Pokemon Encyclopedia</h1>
+            <nav>
+                <ul className='header-nav'>
+                    <li>Placeholder</li>
+                    <li>Placeholder</li>
+                    <li>Placeholder</li>
+                </ul>
+            </nav>
+        </header>
     )
 };
 
