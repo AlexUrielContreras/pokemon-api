19973ec1217fbebd05e02f6ce53273d7c9788bc0
Alex Contreras <alex.code29@gmail.com>
2022-09-22 17:22:59 -0500

    style: add animation to input when focus

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,7 @@ function Header() {
             <h1 className='page-title'>Pokemon Encyclopedia</h1>
             <nav>
                 <ul className='header-nav'>
-                    <li>Placeholder</li>
-                    <li>Placeholder</li>
+                    <li>Login</li>
                     <li>Placeholder</li>
                 </ul>
             </nav>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,10 +5,11 @@ function Homepage() {
         <section className='content-container'>
             <div className='search-container'> 
                 <h2>Search for your Favorite Pokemon</h2>
-                <div className='form-container'>
-                    <input type='text' placeholder='i.e - pikachu'/>
-                    <button type='button'>Search</button>
+                <div className='form-container'>   
+                    <input type='text' autoComplete='off' placeholder='i.e - pikachu'/>                
+                    <span />      
                 </div>
+                <button type='button'>Search</button>
             </div>
         </section>
     )
