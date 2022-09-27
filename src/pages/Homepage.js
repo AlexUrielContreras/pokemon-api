d55eebec3831acde10b3d4c7b2d8b00c1e47b49e
Alex Contreras <alex.code29@gmail.com>
2022-09-27 16:52:38 -0500

    feat: searching pokemon name will fetch pokemon data

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,37 @@
-
+import { useState } from 'react'
+import { fetchPokemon } from '../services/FetchPokemon';
 
 function Homepage() {
+    const [pokeName, setPokeName] = useState('');
+    const [pokeData, setPokeData] = useState('')
+
+    const updateName = (e) => {
+        let name = e.target.value;
+        setPokeName(name)
+    }
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+
+        setPokeName('')
+
+        const response = await fetchPokemon(pokeName);
+        setPokeData(JSON.stringify(response.abilities))
+    }
+
     return (
         <section className='search-container'>
-            <div className='idk'> 
+            <div className='idk'>
                 <h2>Search for your Favorite Pokemon</h2>
-                <div className='form-container'>   
-                    <input type='text' autoComplete='off'/>                
-                    <label></label>      
-                </div>
-                <button type='button'>Search</button>
+                <form onSubmit={handleFormSubmit}>
+                    <div className='form-container'>
+                        <input type='text' autoComplete='off' id='search-pokemon' name='search-pokemon' value={pokeName} onChange={updateName}/>
+                        <label htmlFor='search-pokemon'></label>
+                    </div>
+                    <button type='submit'>Search</button>
+                </form>
             </div>
+            {pokeData}
         </section>
     )
 };
---
+++ src/services/FetchPokemon.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const fetchPokemon = async (pokeName) => {
+    const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+    try {
+       const response = await axios({
+            method: 'get',
+            url: `${baseUrl}${pokeName}`,
+            responseType: 'json'
+        });
+
+        return response.data
+
+    } catch (err) {
+
+        console.error(err)
+    }
+   
+}
